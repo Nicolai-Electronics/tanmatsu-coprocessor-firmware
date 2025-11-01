0aedcc9c388ecc9809769eab5ddc01bc271904a4
z-OS <6945176+z-OS@users.noreply.github.com>
2025-11-01 12:24:02 +0100

    Add leds_init function and update set_led_data_all

--- src/application/leds.h
+++ src/application/leds.h
@@ -2,7 +2,8 @@
 
 #include <stdint.h>
 
-void set_led_data_all(uint8_t* source);
+void leds_init(uint8_t* led_registers);
+void set_led_data_all();
 void set_led_data(uint8_t led_index, uint32_t color);
 void set_power_led(uint32_t color);
 void set_radio_led(uint32_t color);
